80a08366934c2184b9e2c2e6d5947a80db9790ec
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-07-06 16:44:32 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace nav_eshop3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
