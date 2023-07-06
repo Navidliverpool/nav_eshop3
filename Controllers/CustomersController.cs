18ca831334b7fe2285102f960a4636d1c2309d19
navid khodaveisi <navidkhodaveisi2@gmail.com>
2023-07-06 20:46:08 +0100

    On the project I added "New Scaffolded Item"  which I have not been able to do. I was getting error doing the same thing on another projects. Solution: Downgrading one of these packages solved the error: 1.Microsoft.EntityFrameworkCore.Design 2.Microsoft.EntityFrameworkCore.Tools I would say 95% the issue was comming from the first package.

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using nav_eshop3.Data;
 using nav_eshop3.Models;
+using nav_eshop3.ViewModels;
 
 namespace nav_eshop3.Controllers
 {
@@ -27,23 +28,23 @@ namespace nav_eshop3.Controllers
                           Problem("Entity set 'ApplicationDbContext.SalesCustomers'  is null.");
         }
 
-        // GET: Customers/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.SalesCustomers == null)
-            {
-                return NotFound();
-            }
+        //// GET: Customers/Details/5
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null || _context.SalesCustomers == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var salesCustomer = await _context.SalesCustomers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (salesCustomer == null)
-            {
-                return NotFound();
-            }
+        //    var salesCustomer = await _context.SalesCustomers
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (salesCustomer == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(salesCustomer);
-        }
+        //    return View(salesCustomer);
+        //}
 
         // GET: Customers/Create
         public IActionResult Create()
@@ -56,108 +57,114 @@ namespace nav_eshop3.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email")] SalesCustomer salesCustomer)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email")] SalesCustomerVM salesCustomerVM)
         {
             if (ModelState.IsValid)
             {
+                var salesCustomer = new SalesCustomer
+                {
+                    Email = salesCustomerVM.Email,
+                    FirstName = salesCustomerVM.FirstName,
+                    LastName = salesCustomerVM.LastName
+                };
                 _context.Add(salesCustomer);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(salesCustomer);
+            return View(salesCustomerVM);
         }
 
-        // GET: Customers/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.SalesCustomers == null)
-            {
-                return NotFound();
-            }
+        //// GET: Customers/Edit/5
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.SalesCustomers == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var salesCustomer = await _context.SalesCustomers.FindAsync(id);
-            if (salesCustomer == null)
-            {
-                return NotFound();
-            }
-            return View(salesCustomer);
-        }
+        //    var salesCustomer = await _context.SalesCustomers.FindAsync(id);
+        //    if (salesCustomer == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(salesCustomer);
+        //}
 
-        // POST: Customers/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email")] SalesCustomer salesCustomer)
-        {
-            if (id != salesCustomer.Id)
-            {
-                return NotFound();
-            }
+        //// POST: Customers/Edit/5
+        //// To protect from overposting attacks, enable the specific properties you want to bind to.
+        //// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email")] SalesCustomer salesCustomer)
+        //{
+        //    if (id != salesCustomer.Id)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(salesCustomer);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SalesCustomerExists(salesCustomer.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(salesCustomer);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(salesCustomer);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!SalesCustomerExists(salesCustomer.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(salesCustomer);
+        //}
 
-        // GET: Customers/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.SalesCustomers == null)
-            {
-                return NotFound();
-            }
+        //// GET: Customers/Delete/5
+        //public async Task<IActionResult> Delete(int? id)
+        //{
+        //    if (id == null || _context.SalesCustomers == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var salesCustomer = await _context.SalesCustomers
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (salesCustomer == null)
-            {
-                return NotFound();
-            }
+        //    var salesCustomer = await _context.SalesCustomers
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (salesCustomer == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(salesCustomer);
-        }
+        //    return View(salesCustomer);
+        //}
 
         // POST: Customers/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.SalesCustomers == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.SalesCustomers'  is null.");
-            }
-            var salesCustomer = await _context.SalesCustomers.FindAsync(id);
-            if (salesCustomer != null)
-            {
-                _context.SalesCustomers.Remove(salesCustomer);
-            }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirmed(int id)
+        //{
+        //    if (_context.SalesCustomers == null)
+        //    {
+        //        return Problem("Entity set 'ApplicationDbContext.SalesCustomers'  is null.");
+        //    }
+        //    var salesCustomer = await _context.SalesCustomers.FindAsync(id);
+        //    if (salesCustomer != null)
+        //    {
+        //        _context.SalesCustomers.Remove(salesCustomer);
+        //    }
             
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        //    await _context.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
 
-        private bool SalesCustomerExists(int id)
-        {
-          return (_context.SalesCustomers?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
+        //private bool SalesCustomerExists(int id)
+        //{
+        //  return (_context.SalesCustomers?.Any(e => e.Id == id)).GetValueOrDefault();
+        //}
     }
 }
